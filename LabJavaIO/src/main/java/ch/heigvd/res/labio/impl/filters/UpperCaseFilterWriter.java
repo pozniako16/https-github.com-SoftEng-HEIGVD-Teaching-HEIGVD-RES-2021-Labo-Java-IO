8159952d0ccfbf1e6d66ff8a0a43f5399dcfb143
Pozniakoff <lev.pozniakoff@gmail.com>
2021-03-05 16:15:45 +0100

    Did the uppercase filter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -7,27 +7,37 @@ import java.io.Writer;
 /**
  *
  * @author Olivier Liechti
- * test to commit
+ * Modified by Lev POZNIAKOFF
+ *
+ * DATE: 05.03.2021
  */
 public class UpperCaseFilterWriter extends FilterWriter {
   
   public UpperCaseFilterWriter(Writer wrappedWriter) {
     super(wrappedWriter);
   }
-
+  
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.write(str.substring(off, off+len));
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i<off+len; ++i)
+      this.write(cbuf[i]);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (Character.isAlphabetic(c) && c >= (int)'a')
+      super.write(Character.toUpperCase(c));
+    else
+      super.write(c);
   }
 
+  public void write(String s) throws IOException{
+    for(int i = 0; i<s.length(); ++i)
+      this.write(s.charAt(i));
+  }
 }
