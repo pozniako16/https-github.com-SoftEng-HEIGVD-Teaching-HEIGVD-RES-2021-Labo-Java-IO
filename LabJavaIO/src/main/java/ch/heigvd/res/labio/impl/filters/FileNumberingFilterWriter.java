7030f9aee0dc75bdb6f5a1e92728227365db9529
pozniako16 <lev.pozniakoff@gmail.com>
2021-03-09 15:39:08 +0100

    fixing git

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
  * This class transforms the streams of character sent to the decorated writer.
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
- * process.
+ * process.gg
  *
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
@@ -18,24 +18,64 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int lineCount;
+  private boolean isFirstChar;
+  private char lastChar;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    lineCount = 1;
+    isFirstChar = true;
+    lastChar = '\0';
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.write(str.substring(off, off+len));
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i<off+len; i++)
+      this.write(cbuf[i]);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(isFirstChar) {
+      this.writeMultipleDigitsNumber(lineCount++);
+      super.write('\t');
+      isFirstChar = false;
+    }
+
+    if(c == '\n' || lastChar == '\r') {
+      if(lastChar == '\r')
+        super.write(lastChar);
+      if(c == '\n')
+        super.write(c);
+      this.writeMultipleDigitsNumber(lineCount++);
+      super.write('\t');
+      if(lastChar == '\r' && c != '\n')
+        super.write(c);
+    }
+    else if(c != '\r')
+      super.write(c);
+    lastChar = (char) c;
   }
 
+  public void write(String s) throws IOException {
+    for (int i = 0; i < s.length(); ++i) {
+      this.write(s.charAt(i));
+
+    }
+
+  }
+
+  private void writeMultipleDigitsNumber(int i) throws IOException{
+    if(i>=100)
+      super.write('0'+i/100);
+    if(i>=10)
+      super.write('0'+((i%100)/10));
+    super.write('0'+i%10);
+  }
 }
