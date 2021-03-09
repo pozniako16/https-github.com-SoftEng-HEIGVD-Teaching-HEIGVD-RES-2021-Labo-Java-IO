4ad3e2214338bded3ebb134cf4c469bb4bd424d6
Axel Vallon <axel.vallon123@gmail.com>
2021-03-09 10:28:12 +0100

    create loop to copy data from reader to writer

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
@@ -47,11 +47,11 @@ public abstract class FileTransformer implements IFileVisitor {
       Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath()+ ".out"), StandardCharsets.UTF_8); // the bug fix by teacher
       writer = decorateWithFilters(writer);
 
-      /*
-       * There is a missing piece here: you have an input reader and an ouput writer (notice how the 
-       * writer has been decorated by the concrete subclass!). You need to write a loop to read the
-       * characters and write them to the writer.
-       */
+      int b = reader.read();
+      while (b != -1){
+        b = reader.read();
+        writer.write(b);
+      }
       
       reader.close();
       writer.flush();
