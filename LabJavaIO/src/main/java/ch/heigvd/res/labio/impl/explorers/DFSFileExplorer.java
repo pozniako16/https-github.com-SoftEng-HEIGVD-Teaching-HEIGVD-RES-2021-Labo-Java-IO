c7519992313932bd8648a3a6f65ab79da2c93666
Axel Vallon <axel.vallon123@gmail.com>
2021-03-09 10:21:51 +0100

    fix visitor, now pass the tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -10,25 +10,23 @@ import java.io.File;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    //on récupère la liste des fichiers et répertoire
-    File[] listOfFileAndDir = rootDirectory.listFiles();
-    if (listOfFileAndDir == null) //si le repertoire est vide, on quitte
-      return;
+    @Override
+    public void explore(File rootDirectory, IFileVisitor vistor) {
+        //on récupère la liste des fichiers et répertoire
+        vistor.visit(rootDirectory);
+        File[] listOfFileAndDir = rootDirectory.listFiles();
+        if (listOfFileAndDir == null) //si le repertoire est vide, on quitte
+            return;
 
-    for(File file : listOfFileAndDir){
-      if(file.isDirectory())
-        explore(file, vistor); //si on a un repetoire, on le visite
-      else
-        vistor.visit(file); //si on a un fichier, on l'envoie au visitor
+        for (File file : listOfFileAndDir) {
+            explore(file, vistor); //si on a un repertoire, on le visite
+        }
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
     }
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
 
 }
