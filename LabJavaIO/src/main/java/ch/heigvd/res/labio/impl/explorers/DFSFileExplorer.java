3579ea2eb5e71197d992711524d1f375544a5d88
pozniako16 <35344361+pozniako16@users.noreply.github.com>
2021-03-09 16:49:15 +0100

    Refix git

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -1,34 +1,28 @@
 package ch.heigvd.res.labio.impl.explorers;
-
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
-
 import java.io.File;
-
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
-//to do merge
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    //on récupère la liste des fichiers et répertoire
-    File[] listOfFileAndDir = rootDirectory.listFiles();
-    if (listOfFileAndDir == null) //si le repertoire est vide, on quitte
-      return;
 
-    for(File file : listOfFileAndDir){
-      if(file.isDirectory())
-        explore(file, vistor); //si on a un repetoire, on le visite
-      else
-        vistor.visit(file); //si on a un fichier, on l'envoie au visitor
+    @Override
+    public void explore(File rootDirectory, IFileVisitor vistor) {
+        //on récupère la liste des fichiers et répertoire
+        vistor.visit(rootDirectory);
+        File[] listOfFileAndDir = rootDirectory.listFiles();
+        if (listOfFileAndDir == null) //si le repertoire est vide, on quitte
+            return;
+        for (File file : listOfFileAndDir) {
+            explore(file, vistor); //si on a un repertoire, on le visite
+        }
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
     }
-    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
-
 }
